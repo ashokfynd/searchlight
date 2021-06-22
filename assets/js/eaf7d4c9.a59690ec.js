a0d9d3e6b02b7a4d148f17de0e1ffecbcaeae9aa
ashokfynd <ashoknayak@gofynd.com>
2021-06-22 16:56:04 +0530

    Deploy website - based on 4f2cbef91c2062f2ac8d4d5c887614d716808b59

--- assets/js/eaf7d4c9.a59690ec.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[309],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/searchlight/blog/welcome"},{"title":"Hello","permalink":"/searchlight/blog/hello-world"},{"title":"Hola","permalink":"/searchlight/blog/hola"}]}')}}]);
